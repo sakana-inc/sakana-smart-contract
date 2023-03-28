c468e07694757ef5d2c93ae29f1db37ab5502564
gabrielrondon <grondon@gmail.com>
2023-03-28 22:43:55 +0100

    Update Sakana.sol

--- Sakana.sol
+++ Sakana.sol
@@ -1,58 +1,50 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract Sakana {
     struct Idea {
         uint256 id;
         string contentHash;
-        address owner;
     }
 
-    mapping(address => Idea[]) public ideas;
-    uint256 private ideaCounter;
+    mapping(address => mapping(uint256 => Idea)) public ideas;
+    mapping(address => uint256) public ideaCount;
 
     event IdeaRegistered(uint256 indexed id, string contentHash, address indexed owner);
     event IdeaModified(uint256 indexed id, string contentHash, address indexed owner);
     event IdeaDeleted(uint256 indexed id, address indexed owner);
 
     function registerIdea(string memory contentHash) public {
-        ideaCounter++;
-        Idea memory newIdea = Idea(ideaCounter, contentHash, msg.sender);
-        ideas[msg.sender].push(newIdea);
-        emit IdeaRegistered(ideaCounter, contentHash, msg.sender);
+        uint256 newId = ideaCount[msg.sender] + 1;
+        ideas[msg.sender][newId] = Idea(newId, contentHash);
+        ideaCount[msg.sender] = newId;
+
+        emit IdeaRegistered(newId, contentHash, msg.sender);
     }
 
     function retrieveIdea(uint256 id) public view returns (string memory) {
-        Idea[] memory userIdeas = ideas[msg.sender];
-        for (uint256 i = 0; i < userIdeas.length; i++) {
-            if (userIdeas[i].id == id) {
-                return userIdeas[i].contentHash;
-            }
-        }
-        revert("Idea not found");
+        Idea storage idea = ideas[msg.sender][id];
+        require(idea.id != 0, "Idea not found");
+
+        return idea.contentHash;
     }
 
     function modifyIdea(uint256 id, string memory newContentHash) public {
-        Idea[] storage userIdeas = ideas[msg.sender];
-        for (uint256 i = 0; i < userIdeas.length; i++) {
-            if (userIdeas[i].id == id) {
-                userIdeas[i].contentHash = newContentHash;
-                emit IdeaModified(id, newContentHash, msg.sender);
-                return;
-            }
-        }
-        revert("Idea not found");
+        Idea storage idea = ideas[msg.sender][id];
+        require(idea.id != 0, "Idea not found");
+
+        idea.contentHash
+        = newContentHash;
+
+        emit IdeaModified(id, newContentHash, msg.sender);
     }
 
     function deleteIdea(uint256 id) public {
-        Idea[] storage userIdeas = ideas[msg.sender];
-        for (uint256 i = 0; i < userIdeas.length; i++) {
-            if (userIdeas[i].id == id) {
-                userIdeas[i] = userIdeas[userIdeas.length - 1];
-                userIdeas.pop();
-                emit IdeaDeleted(id, msg.sender);
-                return;
-            }
-        }
-        revert("Idea not found");
+        Idea storage idea = ideas[msg.sender][id];
+        require(idea.id != 0, "Idea not found");
+
+        delete ideas[msg.sender][id];
+
+        emit IdeaDeleted(id, msg.sender);
     }
 }
